5a7cc6945979315c39a553168349d6c8054b04ef
vivekyadav1414 <147232951+vivekyadav1414@users.noreply.github.com>
2024-10-02 12:11:20 +0530

    Add project files.

---
+++ GYMandYOGA/Controllers/AdminZoneController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GYMandYOGA.Models;
+
+namespace GYMandYOGA.Controllers
+{
+    public class AdminZoneController : Controller
+    {
+        //
+        // GET: /AdminZone/
+        MPROGYMANDYOGAEntities db = new MPROGYMANDYOGAEntities();
+
+        public ActionResult Index()
+        {
+            if (Session["aid"] != null)
+            {
+
+            }
+            else
+            {
+                Response.Write("<script>alert('LogIn First then go to Next Zone');window.location.href='/Home/login'</script>");
+            }
+            return View();
+        }
+        public ActionResult ContactMGMT()
+        {
+            if (Session["aid"] != null)
+            {
+
+            }
+            else
+            {
+                Response.Write("<script>alert('LogIn First then go to Next Zone');window.location.href='/Home/login'</script>");
+            }
+            List<TBL_Contact> lst = null;
+            lst = db.TBL_Contact.ToList();
+            return View(lst);
+        }
+        public ActionResult MemberMGMT()
+        {
+            if (Session["aid"] != null)
+            {
+
+            }
+            else
+            {
+                Response.Write("<script>alert('LogIn First then go to Next Zone');window.location.href='/Home/login'</script>");
+            }
+            List<TBL_Members> lst = null;
+            lst = db.TBL_Members.ToList();
+
+            return View(lst);
+        }
+
+
+        public ActionResult ChangePassword()
+        {
+            return View();
+        }
+       [HttpPost]
+        public ActionResult ChangePassword(string oldpass, string newpass, string confirmpass)
+     {
+         if (newpass == confirmpass)
+         {
+             string UserId = Session["aid"].ToString();
+             TBL_Login lg = db.TBL_Login.Where(x => x.Password == oldpass && x.UserId == x.UserId).SingleOrDefault();
+             lg.Password = newpass;
+             db.SaveChanges();
+             Response.Write("<script>alert('Your Password Changed');window.location.href='/Home/login'</script>");
+         }
+         else
+         {
+             Response.Write("<script>alert('Please Enter ')</script>");
+         }
+           return View();
+       }
+
+        public void logout()
+        {
+            Session.Abandon();
+            Response.Write("<script>alert('LogOut');window.location.href='/Home/login'</script>");
+        }
+
+        public void Delete()
+        {
+            try
+            {
+                string m = Request.QueryString["m"];
+                TBL_Members TBL = db.TBL_Members.SingleOrDefault(X => X.mob == m);
+                db.TBL_Members.Remove(TBL);
+                db.SaveChanges();
+                Response.Write("<script>alert('Record Delete Successfull');window.location.href='/AdminZone/MemberMGMT'</script>");
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('Record Not Delete ')</script>");
+            }
+        }
+        [HttpGet]
+        public ActionResult UpdateRecord()
+        {
+            string aid = Request.QueryString["U"];
+            TBL_Members reg = db.TBL_Members.SingleOrDefault(a => a.Name == aid);
+            return View(reg);
+        }
+        [HttpPost]
+        public void UpdateRecord(TBL_Members reg,string mob)
+        {
+            TBL_Members rg = db.TBL_Members.SingleOrDefault(a => a.mob == mob);
+            try
+            {
+                rg.Name = reg.Name;
+                rg.mob = reg.mob;
+                rg.address = reg.address;
+                rg.city = reg.city;
+                rg.pplan = reg.pplan;
+                rg.gender = reg.gender;
+                rg.Regdate = DateTime.Now.ToString();
+                db.SaveChanges();
+                Response.Write("<script>alert('Record Update Successfully');window.location.href='/AdminZone/MemberMGMT'</script>");
+            }
+            catch(Exception ex)
+            {
+                Response.Write("<script>alert('Record Not Update')</script>");
+            }
+
+        }
+        
+    }
+}
---
+++ GYMandYOGA/Controllers/HomeController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GYMandYOGA.Models;
+
+namespace GYMandYOGA.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        MPROGYMANDYOGAEntities db = new MPROGYMANDYOGAEntities();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+       
+      
+        public ActionResult contact()    // Get Action run of view page
+        {
+            return View();
+        }
+
+       [HttpPost]
+        public ActionResult contact(TBL_Contact con)
+        {
+            try
+            {
+                con.contactdate = DateTime.Now.ToString();
+                db.TBL_Contact.Add(con);
+                db.SaveChanges();
+                Response.Write("<script>alert('Record Save Successfully') </script>");
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('Record not Save') </script>");
+            }
+
+            return View();
+        }
+
+        public ActionResult picture()
+        {
+            return View();
+        }
+
+
+        public ActionResult video()
+        {
+            return View();
+        }
+
+        public ActionResult member()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult member(TBL_Members reg,string hdn1,string ct1)
+        {
+            try
+            {
+                if (hdn1 == ct1)
+                {
+                    reg.Regdate = DateTime.Now.ToString();
+                    db.TBL_Members.Add(reg);
+                    db.SaveChanges();
+                    Response.Write("<script>alert('Record Save Successfully') </script>");
+                }
+                else
+                {
+                    Response.Write("<script>alert('Invalid Captcha Code')</script>");
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('Record not Save') </script>");
+            }
+
+            return View();
+        }
+
+        public ActionResult login()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult login(TBL_Login lg)
+        {
+            try
+            {
+                TBL_Login t1 = db.TBL_Login.Where(x => x.UserId == lg.UserId && x.Password == lg.Password).SingleOrDefault();
+                if (t1 != null)
+                {
+                    Session["aid"]=lg.UserId;  // set of session
+                    Response.Write("<script>alert('Welcome to Admin Zone');window.location.href='/AdminZone/Index'</script>");
+                }
+                else
+                {
+                    Response.Write("<script>('Invailid')</script>");
+                }
+
+            }
+            catch (Exception ex)
+            { 
+            
+            }
+            return View();
+        }
+
+        public ActionResult review()
+        {
+            return View();
+        }
+
+       
+        }
+
+    }
+
